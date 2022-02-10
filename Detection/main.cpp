00aa77fb6e18dfdbe5c1f87e637ae1f44b1de003
Liucheng Zhu <13927181971@163.com>
2022-02-10 13:28:43 +0800

    The 4th commit

--- Detection/main.cpp
+++ Detection/main.cpp
@@ -6,6 +6,8 @@
 using namespace std;
 using namespace cv;
 
+double calcPCAOrientation(vector<Point> &pts, Mat &image);
+
 int main(int argv, char** argc)
 {
 	string path = "./image/pca/pca_test1.jpg";
@@ -34,9 +36,28 @@ int main(int argv, char** argc)
 		double area = contourArea(contours[i]);
 		if (area > 1e5 || area < 1e2) continue;
 		drawContours(result, contours, i, Scalar(0, 0, 255), 2, 8);
+		double theta = calcPCAOrientation(contours[i], result);
 	}
 	imshow("contours result", result);
 
 	waitKey(0);
 	return 0;
+}
+
+double calcPCAOrientation(vector<Point>& pts, Mat& image)
+{
+	int size = static_cast<int>(pts.size());
+	Mat data_pts = Mat(size, 2, CV_64FC1);
+	for (int i = 0; i < size; i++)
+	{
+		data_pts.at<double>(i, 0) = pts[i].x;
+		data_pts.at<double>(i, 1) = pts[i].y;
+	}
+
+	// perform PCA projection
+	PCA pca_analysis(data_pts, Mat(), PCA::DATA_AS_ROW);
+	Point cnt = Point(static_cast<int>(pca_analysis.mean.at<double>(0, 0)), static_cast<int>(pca_analysis.mean.at<double>(0, 1)));
+	circle(image, cnt, 2, Scalar(0, 255, 0), 2, 8, 0);
+
+	return 0.0;
 }
